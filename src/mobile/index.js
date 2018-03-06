68bac6a3a11ba983e1b7c987d33d06e7c1579f3d
Cleirton Viana <37007791+csviana@users.noreply.github.com>
2018-03-06 16:40:24 -0300

    Create index.js

---
+++ src/mobile/index.js
@@ -0,0 +1,6 @@
+/**
+ * File: index.js
+ * Description:  Script raiz do diretório /src/mobile dentro do repositório https://github.com/csviana/DVM
+ * Author: Cleirton Viana
+ * Create Date: 06/03/2017
+ */
