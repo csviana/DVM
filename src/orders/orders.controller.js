88d912f3b433b4b0626788fa485a626867845e19
csviana <cleirtonviana@gmail.com>
2018-03-06 19:47:12 -0300

    some message goes here

---
+++ src/orders/orders.controller.js
@@ -0,0 +1,6 @@
+/**
+ * File: orders.controller.js
+ * Description:  Script responsável pelo controle do produto - repositório https://github.com/csviana/DVM
+ * Author: Cleirton Viana
+ * Create Date: 06/03/2017
+ */
