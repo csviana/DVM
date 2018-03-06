1e70f715dc32aae27486e26ba3b0649e3d124bc0
Cleirton Viana <37007791+csviana@users.noreply.github.com>
2018-03-06 16:50:29 -0300

    Create index.js

---
+++ src/web/index.js
@@ -0,0 +1,6 @@
+/**
+ * File: index.js
+ * Description:  Script raiz do diretório /src/web dentro do repositório https://github.com/csviana/DVM
+ * Author: Cleirton Viana
+ * Create Date: 06/03/2017
+ */
