7ed7f15334b36ec07412547ff66717fc276125f4
Cleirton Viana <37007791+csviana@users.noreply.github.com>
2018-03-06 16:55:14 -0300

    Create products.controller.js

---
+++ src/products/products.controller.js
@@ -0,0 +1,6 @@
+/**
+ * File: products.controller.js
+ * Description:  Script responsável pelo controle do produto - repositório https://github.com/csviana/DVM
+ * Author: Cleirton Viana
+ * Create Date: 06/03/2017
+ */
