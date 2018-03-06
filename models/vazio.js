4230f597851e70f79fe84331b0b58eae851b02b1
Cleirton Viana <37007791+csviana@users.noreply.github.com>
2018-03-06 15:50:51 -0300

    Create vazio.js

---
+++ models/vazio.js
@@ -0,0 +1,6 @@
+/**
+ * File: vazio.js
+ * Description:  Marcador do diretório /models dentro do repositório https://github.com/csviana/DVM
+ * Author: Cleirton Viana
+ * Create Date: 06/03/2017
+ */
